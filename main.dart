95073dde3819ed1ab34a140facdf92c7d417551b
bhuvaneshwari-ai <megavathbhuvaneshwari@gmail.com>
2024-11-06 12:35:10 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,154 +1,128 @@
-import 'package:flutter/material.dart';
+import'package:flutter/material.dart';
 void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
+  Widget build(BuildContext context){
     return MaterialApp(
-      home: Scaffold(  
-        appBar: AppBar(title: Text('Flutter Form Example')),
-        body: MyForm(),
+      title:'Responsive UI',
+      theme: ThemeData(
+        primarySwatch:Colors.blue,
+      ),
+      home:ResponsiveHomePage(),
+      );
+  }
+}
+class ResponsiveHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(  
+        title:Text('Responsive UI Design'),
+      ),
+      body: LayoutBuilder(
+        builder:(context,constraints){
+          if(constraints.maxWidth>=1200){
+            return DesktopLayout();
+          }else if(constraints.maxWidth>800 &&
+          constraints.maxWidth<=1200){
+            return TabletLayout();
+          }else{
+            return MobileLayout();
+          }
+        },
+      ),
+bottomNavigationBar: Footer(),
+    );
+  }
+}
+class MobileLayout extends StatelessWidget{
+   @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Header(),
+        Expanded(child: CardGridView(crossAxisCount:2)),
+      ],
+    );
+}
+}
+class TabletLayout extends StatelessWidget{
+   @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Header(),
+        Expanded(child: CardGridView(crossAxisCount:3)),
+      ],
+    );
+}
+}
+class DesktopLayout extends StatelessWidget{
+   @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Header(),
+        Expanded(child: CardGridView(crossAxisCount:4)),
+      ],
+    );
+}
+}
+class Header extends StatelessWidget{
+   @override
+  Widget build(BuildContext context){
+    return Container(
+      padding:EdgeInsets.all(16.0),
+      color:Colors.blue,
+      child: Center(
+        child:Text(
+          'this is BHUNA',
+          style:TextStyle(fontSize: 64,color: Colors.black),
+        ),
       ),
     );
   }
 }
-class MyForm extends StatefulWidget{
+class CardGridView extends StatelessWidget{
+  final int crossAxisCount;
+  CardGridView({required this.crossAxisCount});
   @override
-  _MyFormState createState()=>_MyFormState();
-}
-class _MyFormState extends State<MyForm>{
-  final _formKey=GlobalKey<FormState>();
-  String? _name;
-  String? _email;
-  String? _gender;
-bool _isTermsAccepted=false;
-@override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key:_formKey,
-        child:Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children:<Widget>[
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value){
-                if(value==null || value.isEmpty){
-                  return 'please enter your name';
-                }
-                return null;
-              },
-              onSaved:(value){
-                _name=value;
-              },
+    return GridView.builder(
+      gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount:crossAxisCount,
+        crossAxisSpacing:10.0,
+        mainAxisSpacing:10.0,
+      ),
+      padding:EdgeInsets.all(10.0),
+      itemCount: 10,
+      itemBuilder: (context,index){
+        return Card(
+          color:Colors.blueAccent,
+          child: Center(
+            child:Text(
+              'Card $index',
+              style: TextStyle(fontSize:64,color: Colors.black),
             ),
-  
-            SizedBox(height: 16),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value){
-                if(value==null || value.isEmpty){
-                  return 'please enter your email';
-                }
-                if(!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)){
-                  return 'please enter a valid email address';
-                }
-                return null;
-              },
-              onSaved:(value){
-                _email=value;
-              },
             ),
-            SizedBox(height: 16),
-            Text('Gender',style:TextStyle(fontSize: 16)),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: RadioListTile<String>(
-                    title: const Text('Male'),
-                    value: 'Male',
-                    groupValue: _gender,
-                    onChanged: (value){
-                    setState((){
-                      _gender=value;
-                    });
-  },
-                  ),
-                  ),
-                 Expanded(
-                  child: RadioListTile<String>(
-                    title: const Text('Female'),
-                    value: 'Female',
-                    groupValue: _gender,
-                    onChanged: (value){
-                    setState((){
-                      _gender=value;
-                    });
-},
-),
-            ),
-            ],
-            ),
-            SizedBox(height: 16),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(
-                labelText: 'Country',
-                border: OutlineInputBorder(),
-              ),
-              items: <String>['INDIA','Canada','Mexico']
-              .map<DropdownMenuItem<String>>((String value){
-                return DropdownMenuItem<String>(
-                value:value,
-                child:Text(value),
-                );
-                  }).toList(),
-                  onChanged: (value){
-                    setState(() {
-                      
-                    });
-
-                },
-                validator: (value){
-                  if(value==null || value.isEmpty){
-                    return 'please select your country';
-                  }
-                  return null;
-              },),
-            SizedBox(height: 16),
-           CheckboxListTile(
-            title: Text('I accept the terms and conditions'),
-            value: _isTermsAccepted,
-            onChanged: (bool? value){
-              setState(() {
-                _isTermsAccepted=value?? false;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-            contentPadding: EdgeInsets.zero,
-           ),
-           SizedBox(height: 16),
-           ElevatedButton(
-            onPressed: (){
-       if(_formKey.currentState!.validate()){
-        _formKey.currentState!.save();
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content:Text('Form Successfully submitted!')),
         );
-       }
-            },
-            child: Text('Submit'),
-           ),
-          ],
+      },
+    );
+  }
+}
+class Footer extends StatelessWidget{
+   @override
+  Widget build(BuildContext context){
+    return Container(
+      padding:EdgeInsets.all(16.0),
+      color:Colors.blue,
+      child: Center(
+        child:Text(
+          'this is BHUNA',
+          style:TextStyle(color: Colors.black),
         ),
       ),
     );
